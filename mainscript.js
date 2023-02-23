c839e3223f5fa05c7199cb8ccfec299f01c00a1a
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-23 11:13:07 +0100

    changes to main script

--- mainscript.js
+++ mainscript.js
@@ -1,7 +1,7 @@
 "use strict"
 
-import {capitalize, makeSpaces} from "./stringscript.js"; 
-// import * as stringTools from "./stringscript.js";
+// import {capitalize, makeSpaces} from "./stringscript.js"; 
+import * as stringTools from "./stringscript.js";
 
 // we don't need this because modules promise that the DOM is fully loaded before run the script. In addition, you can move script links up in the HTML (take a look at the HTML)
 // window.addEventListener("DOMContentLoaded", start);ù
@@ -11,12 +11,12 @@ start();
 
 function start() {
   console.log("Start!");
-  sayHello("peter");
+  sayHello("Klaus");
 }
 
 function sayHello( name ) {
-  const greeting = makeSpaces(`Hello ${capitalize(name)}!`);
-  // const greeting = stringTools.makeSpaces(`Hello ${stringTools.capitalize(name)}!`);
+  // const greeting = makeSpaces(`Hello ${capitalize(name)}!`);
+  const greeting = stringTools.makeSpaces(`Yo ${stringTools.capitalize(name)}!`);
   document.querySelector("#output").textContent = greeting;
   console.log("greeting:", greeting);
 }
