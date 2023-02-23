aa66aef51fb33dc02188dff973621adf621cf371
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-23 11:14:19 +0100

    changes to manster

--- mainscript.js
+++ mainscript.js
@@ -1,12 +1,6 @@
 "use strict"
 
-// import {capitalize, makeSpaces} from "./stringscript.js"; 
-import * as stringTools from "./stringscript.js";
-
-// we don't need this because modules promise that the DOM is fully loaded before run the script. In addition, you can move script links up in the HTML (take a look at the HTML)
-// window.addEventListener("DOMContentLoaded", start);ù
-// MODULES ONLY RUN ON LIVE SERVER OR SERVERS
-
+import {capitalize, makeSpaces} from "./stringscript.js"; 
 start();
 
 function start() {
@@ -15,8 +9,7 @@ function start() {
 }
 
 function sayHello( name ) {
-  // const greeting = makeSpaces(`Hello ${capitalize(name)}!`);
-  const greeting = stringTools.makeSpaces(`Hello ${stringTools.capitalize(name)}!`);
+  const greeting = makeSpaces(`Hello ${capitalize(name)}!`);
   document.querySelector("#output").textContent = greeting;
   console.log("greeting:", greeting);
 }
