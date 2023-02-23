45deab8af07d4892bb32b58a5b5f921410d2c1ee
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-23 11:15:46 +0100

    changes to mainscript

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,5 @@
 "use strict"
 
-// import {capitalize, makeSpaces} from "./stringscript.js"; 
 import * as stringTools from "./stringscript.js";
 
 start();
@@ -11,7 +10,6 @@ function start() {
 }
 
 function sayHello( name ) {
-  // const greeting = makeSpaces(`Hello ${capitalize(name)}!`);
   const greeting = stringTools.makeSpaces(`Yo ${stringTools.capitalize(name)}!`);
   document.querySelector("#output").textContent = greeting;
   console.log("greeting:", greeting);
